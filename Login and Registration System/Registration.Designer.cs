9b7175fb806750aab31de385fc77e84bc8866e70
Kevin Tran <kevintran1142@gmail.com>
2022-03-26 12:15:37 -0700

    Add project files.

---
+++ Login and Registration System/Registration.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Login_and_Registration_System
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameTxt = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.confirmPassword = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userNameTxt
+            // 
+            this.userNameTxt.AutoSize = true;
+            this.userNameTxt.Location = new System.Drawing.Point(237, 64);
+            this.userNameTxt.Name = "userNameTxt";
+            this.userNameTxt.Size = new System.Drawing.Size(88, 13);
+            this.userNameTxt.TabIndex = 0;
+            this.userNameTxt.Text = "Enter User Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(365, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // passwordText
+            // 
+            this.passwordText.AutoSize = true;
+            this.passwordText.Location = new System.Drawing.Point(237, 107);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(81, 13);
+            this.passwordText.TabIndex = 0;
+            this.passwordText.Text = "Enter Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // confirmPassword
+            // 
+            this.confirmPassword.AutoSize = true;
+            this.confirmPassword.Location = new System.Drawing.Point(237, 161);
+            this.confirmPassword.Name = "confirmPassword";
+            this.confirmPassword.Size = new System.Drawing.Size(91, 13);
+            this.confirmPassword.TabIndex = 0;
+            this.confirmPassword.Text = "Confirm Password";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(365, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(155, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Register Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Have Account ? Login Now";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.confirmPassword);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.userNameTxt);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Registration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameTxt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label passwordText;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label confirmPassword;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Login and Registration System/Registration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Login_and_Registration_System
+{
+    public partial class Registration : Form
+    {
+        private SqlDataReader dr;
+
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Coding\Login and Registration System\Login and Registration System\Database.mdf';Integrated Security=True");
+            con.Open(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            // Login login = new Login();
+            //login.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Coding\Login and Registration System\Login and Registration System\Database.mdf';Integrated Security=True");
+            con.Open(); 
+            if(confirmPassword.Text != string.Empty || passwordText.Text != string.Empty)
+            {
+                if (passwordText.Text == confirmPassword.Text)
+                {
+                    SqlCommand cmd = new SqlCommand("select * from LoginTable where username='" + userNameTxt.Text + "'", con);
+                    dr = cmd.ExecuteReader();
+                    if(dr.Read())
+                    {
+                        dr.Close();
+                        MessageBox.Show("Username already exist, please try another ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    } else
+                    {
+                        dr.Close();
+                        cmd = new SqlCommand("insert into LoginTable values(@username,@password)", con);
+                        cmd.Parameters.AddWithValue("password", passwordText.Text);
+                        cmd.Parameters.AddWithValue("username", userNameTxt.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Your Account is created . Please Login now.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                } 
+                else
+                {
+                    MessageBox.Show("Please enter both password same ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
